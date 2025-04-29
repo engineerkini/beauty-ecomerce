aae266ee7a6eab42785d21ff82e0cf94f4eba2ce
engineerkini <engineerkini@gmail.com>
2025-04-29 05:50:39 -0400

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import Hero from '../components/layouts/Hero'
-import NewArrivals from '../components/layouts/NewArrivals'
-import BestSellers from '../components/layouts/BestSellers'
-import ProductShowcase from '../components/layouts/ProductShowcase'
-import OnTheBlog from '../components/layouts/OnTheBlog'
-
-
+import React from "react";
+import Hero from "../components/layouts/Hero";
+import NewArrivals from "../components/layouts/NewArrivals";
+import BestSellers from "../components/layouts/BestSellers";
+import ProductShowcase from "../components/layouts/ProductShowcase";
+import OnTheBlog from "../components/layouts/OnTheBlog";
 
 const Home = () => {
   return (
-     <div className='flex flex-col gap-[80px] md:gap-[60px] 2xl:gap-[80px] mt-16'>
-      <Hero/>
+    <div className="flex flex-col gap-[80px] md:gap-[60px] 2xl:gap-[80px] mt-16">
+      <Hero />
       <NewArrivals />
       <BestSellers />
       <ProductShowcase />
       <OnTheBlog />
-    
-     </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
